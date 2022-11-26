0fad495f8fe93e51872f3e1b042f88858e228f6e
Camilo Ernesto Balanta Torres <92347521+ManoNogra87@users.noreply.github.com>
2022-11-26 13:59:58 -0500

    Update Proyecto1.java

--- Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.proyecto1;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- *
- * @author Dell
- */
 public class Proyecto1 {
 
     public static void main(String[] args) throws IOException {
@@ -22,7 +13,7 @@ public class Proyecto1 {
         Integer followerinstagrat = 100;
         Integer youtube = 200;
         String seguidores = " Tengo "+followerinstagrat+ " en instagrant"+
-                " y en Youtube "+youtube+" en total tengo "+(followerinstagrat+youtube)+""; 
+        " y en Youtube "+youtube+" en total tengo "+(followerinstagrat+youtube)+""; 
         System.out.println(frace+nombre+","+seguidores);
         String operacion = "Estamos trabajado desde el ";
         Integer ano1 = 2008;
@@ -35,9 +26,9 @@ public class Proyecto1 {
         //capturando el dato
         Integer edad = captura.nextInt();
         if (edad > 18){
-        System.out.println("Es mayor de 18");
-        } else {
-            System.out.println("Es menor de edad");
+            System.out.println("Es mayor de 18");
+            } else {
+                System.out.println("Es menor de edad");
         }       
         System.out.println("Ingrese su edad de nuevo");
         //creando el lector
@@ -46,10 +37,9 @@ public class Proyecto1 {
         String valor = br.readLine();
         //evaluendo el dato
         if (Integer.parseInt(valor) > 18){
-            System.out.println("El numero ingresado es mayor de 18");
-        } else {
-            System.out.println("El numero ingresado es menor de 18");
-        }
-                
+                System.out.println("El numero ingresado es mayor de 18");
+            } else {
+                System.out.println("El numero ingresado es menor de 18");
+        }                
     }   
     }
